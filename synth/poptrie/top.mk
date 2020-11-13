c7e84a5ba1bc51522568530a72bbb4b492f883f2
tamimcse <tamim@csebuet.org>
2020-11-12 23:20:38 -0500

    .

--- synth/poptrie/top.mk
+++ synth/poptrie/top.mk
@@ -1,8 +1,8 @@
 export DESIGN_NAME = top
 export PLATFORM    = nangate45
 
-export VERILOG_FILES = ./designs/src/poptrie/poptrie.v
-export SDC_FILE      = ./designs/src/poptrie/poptrie.sdc
+export VERILOG_FILES = ./designs/src/poptrie/top.v
+export SDC_FILE      = ./designs/src/poptrie/top.sdc
 
 # These values must be multiples of placement site
 # x=0.19 y=1.4
