44525211d07df61e8bf867a545cde6d93bda534d
tamimcse <tamim@csebuet.org>
2021-01-01 01:10:01 -0500

    .

--- module_library.v
+++ module_library.v
@@ -494,6 +494,44 @@ module WIDE_MUL_GATE(clock, in1, in2, out1);
   MUL_GATE #(.BITSIZE_in1(BITSIZE_in1), .BITSIZE_in2(BITSIZE_in2), .BITSIZE_out1(BITSIZE_out1), .PIPE_PARAMETER(PIPE_PARAMETER)) m1 (.out1(out1), .clock(clock), .in1(in1), .in2(in2));
 endmodule
 
+//This is adopted from:
+// https://verilogcodes.blogspot.com/2015/11/synthesisable-verilog-code-for-division.html
+`timescale 1ns / 1ps
+module DIV_GATE(A,B,Res);
+
+    //the size of input and output ports of the division module is generic.
+    parameter WIDTH = 8;
+    //input and output ports.
+    input [WIDTH-1:0] A;
+    input [WIDTH-1:0] B;
+    output [WIDTH-1:0] Res;
+    //internal variables    
+    reg [WIDTH-1:0] Res = 0;
+    reg [WIDTH-1:0] a1,b1;
+    reg [WIDTH:0] p1;   
+    integer i;
+
+    always@ (A or B)
+    begin
+        //initialize the variables.
+        a1 = A;
+        b1 = B;
+        p1= 0;
+        for(i=0;i < WIDTH;i=i+1)    begin //start the for loop
+            p1 = {p1[WIDTH-2:0],a1[WIDTH-1]};
+            a1[WIDTH-1:1] = a1[WIDTH-2:0];
+            p1 = p1-b1;
+            if(p1[WIDTH-1] == 1)    begin
+                a1[0] = 0;
+                p1 = p1 + b1;   end
+            else
+                a1[0] = 1;
+        end
+        Res = a1;   
+    end 
+
+endmodule
+
 // This component is part of the BAMBU/PANDA IP LIBRARY
 // Copyright (C) 2004-2020 Politecnico di Milano
 // Author(s): Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
