441b82c02b3100d252e9a4c0b5e6c5b19c61d135
tamimcse <tamim@csebuet.org>
2021-01-05 01:14:47 -0500

    .

--- tests/rcp.c
+++ tests/rcp.c
@@ -8,31 +8,32 @@ MD Iftakharul Islam et.al. "Leveraging Domino to Implement RCP in a Stateful Pro
 #define ALPHA (1)
 #define BETA (1/2)
 
-//states[0] = Running average of RTT in ms
-//states[1] = Number of Bytes received
-//states[2] = RCP feedback rate in MB/s
-//states[3] = clock tick when the RCP rate has been calculated
+//rtt_a: Running average of RTT in ms
+//bytes_received: Number of Bytes received
+//feedback_rate: RCP feedback rate in MB/s
+//last_clock_tick = clock tick when the RCP rate has been calculated
 //rtt: is the RTT of the packet
 //tick: is the current time
 //queue: current queue size
 //packet_size: size of the packet in bytes
 //control_interval: control interval in ms 
 //capacity: capacity in megabytes
-uint32_t rcp(uint32_t rtt, uint32_t tick, uint32_t queue, uint32_t packet_size, int control_interval, int capacity, uint32_t states[]) {
+uint32_t rcp(uint32_t rtt, uint32_t tick, uint32_t queue, uint32_t packet_size, int control_interval, int capacity,
+ uint32_t *rtt_a, uint32_t *bytes_received, uint32_t *feedback_rate, uint32_t *last_clock_tick) {
   uint32_t spare_capacity;
   
-  if ((tick - states[3]) > control_interval) {
-    spare_capacity = capacity - states[1] / (1000 * control_interval);
-    states[0] = (states[0] * 49 + rtt)/50;
-    states[1] = 0;
-    states[2] *= 1 + ((control_interval/states[0]) * (ALPHA * spare_capacity - (BETA * queue)/states[0]))/capacity;
-    states[3] = tick;
-    return states[2];
+  if ((tick - *last_clock_tick) > control_interval) {
+    spare_capacity = capacity - *bytes_received / (1000 * control_interval);
+    *rtt_a = (*rtt_a * 49 + rtt)/50;
+    *bytes_received = 0;
+    *feedback_rate *= 1 + ((control_interval/(*rtt_a)) * (ALPHA * spare_capacity - (BETA * queue)/(*rtt_a)))/capacity;
+    *last_clock_tick = tick;
+    return *feedback_rate;
   }
   else {
-    states[0] = (states[0] * 49 + rtt)/50;
-    states[1] += packet_size;
-    return states[2];
+    *rtt_a = (*rtt_a * 49 + rtt)/50;
+    *bytes_received += packet_size;
+    return *feedback_rate;
   }
 }
 
