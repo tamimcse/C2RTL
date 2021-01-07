fa42ef1015c4e9828b30f8cc7c17ee5370c61d35
tamimcse <tamim@csebuet.org>
2021-01-07 00:58:38 -0500

    .

--- tests/ethernet_forwarding.c
+++ tests/ethernet_forwarding.c
@@ -3,22 +3,21 @@ This code has been take from Domino project. This has been adopted from:
 https://github.com/packet-transactions/domino-examples/blob/master/domino_programs/heavy_hitters.c
 */
 #include "hashes.h"
+#include <stdint.h>
 #include <stdbool.h>
 
-#define low_th 100
-#define hi_th  1000
 
-#define NUM_ENTRIES 4096
+#define NUM_ENTRIES 65536
 
-bool count_min_sketch(int sport, int dport, int sketch_cnt_1[NUM_ENTRIES], 
-  int sketch_cnt_2[NUM_ENTRIES], int sketch_cnt_3[NUM_ENTRIES]) {
-  int sketch1_idx;
-  int sketch2_idx;
-  int sketch3_idx;
+bool count_min_sketch(uint64_t mac_addr, uint32_t hash1[NUM_ENTRIES],
+     uint32_t hash2[NUM_ENTRIES], uint32_t hash3[NUM_ENTRIES]) {
+  int hash1_idx;
+  int hash2_idx;
+  int hash3_idx;
 
-  sketch1_idx = hash2a(sport, dport) % NUM_ENTRIES;
-  sketch2_idx = hash2b(sport, dport) % NUM_ENTRIES;
-  sketch3_idx = hash2c(sport, dport) % NUM_ENTRIES;
+  hash1 = hash2a(sport, dport) % NUM_ENTRIES;
+  hash2 = hash2b(sport, dport) % NUM_ENTRIES;
+  hash3 = hash2c(sport, dport) % NUM_ENTRIES;
 
   sketch_cnt_1[sketch1_idx]+= 1;
   sketch_cnt_2[sketch2_idx]+= 1;
